e2a343f341ce8755e7855abd2a2d4b2356752c9c
SerjiVutinss <SerjiVutinss@gmail.com>
2019-10-22 19:38:03 +0100

    Created PasswordServer and PasswordServiceImpl skeleton

---
+++ src/main/java/ie/gmit/serji/PasswordServiceImpl.java
@@ -0,0 +1,9 @@
+package ie.gmit.serji;
+
+import ie.gmit.serji.passwordservice.PasswordServiceGrpc;
+
+public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImplBase {
+
+
+
+}
